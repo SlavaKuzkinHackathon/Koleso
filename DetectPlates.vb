003b88613941923f552a7a132b935045382594eb
Вячеслав Кузькин <Slava835@live.com>
2020-06-10 13:49:11 +0300

    Добавьте файлы проекта.

---
+++ DetectPlates.vb
@@ -0,0 +1,217 @@
+﻿'DetectPlates.vb
+'
+'Emgu CV 3.0.0
+
+Option Explicit On      'require explicit declaration of variables, this is NOT Python !!
+Option Strict On        'restrict implicit data type conversions to only widening conversions
+
+Imports Emgu.CV                     '
+Imports Emgu.CV.CvEnum              'Emgu Cv imports
+Imports Emgu.CV.Structure           '
+Imports Emgu.CV.UI                  '
+Imports Emgu.CV.Util                '
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Module DetectPlates
+
+    ' module level variables ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    Const PLATE_WIDTH_PADDING_FACTOR As Double = 1.3 '3.0
+    Const PLATE_HEIGHT_PADDING_FACTOR As Double = 1.5 '3.0
+
+    Dim SCALAR_WHITE As New MCvScalar(255.0, 255.0, 255.0)
+    Dim SCALAR_RED As New MCvScalar(0.0, 0.0, 255.0)
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    Function detectPlatesInScene(imgOriginalScene As Mat) As List(Of PossiblePlate)
+        Dim listOfPossiblePlates As List(Of PossiblePlate) = New List(Of PossiblePlate)         'this will be the return value
+
+        Dim imgGrayscaleScene As New Mat()
+        Dim imgThreshScene As New Mat()
+        Dim imgContours As New Mat(imgOriginalScene.Size, DepthType.Cv8U, 3)
+
+        Dim random As New Random()
+
+        CvInvoke.DestroyAllWindows()
+
+        If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''''''
+            CvInvoke.Imshow("0", imgOriginalScene)
+        End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        Preprocess.preprocess(imgOriginalScene, imgGrayscaleScene, imgThreshScene)          'preprocess to get grayscale and threshold images
+
+        If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''''''
+            CvInvoke.Imshow("1a", imgGrayscaleScene)
+            CvInvoke.Imshow("1b", imgThreshScene)
+        End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        'find all possible chars in the scene,
+        'this function first finds all contours, then only includes contours that could be chars (without comparison to other chars yet)
+        Dim listOfPossibleCharsInScene As List(Of PossibleChar) = findPossibleCharsInScene(imgThreshScene)
+
+        If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''''''
+            Koleso.txtInfo.AppendText("step 2 - listOfPossibleCharsInScene.Count = " + listOfPossibleCharsInScene.Count.ToString + vbCrLf)     '131 with MCLRNF1 image
+
+            Dim contours As New VectorOfVectorOfPoint()
+
+            For Each possibleChar As PossibleChar In listOfPossibleCharsInScene
+                contours.Push(possibleChar.contour)
+            Next
+
+            CvInvoke.DrawContours(imgContours, contours, -1, SCALAR_WHITE)
+            CvInvoke.Imshow("2b", imgContours)
+        End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        'given a list of all possible chars, find groups of matching chars
+        'in the next steps each group of matching chars will attempt to be recognized as a plate
+        Dim listOfListsOfMatchingCharsInScene As List(Of List(Of PossibleChar)) = findListOfListsOfMatchingChars(listOfPossibleCharsInScene)
+
+        If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''''''
+            Koleso.txtInfo.AppendText("step 3 - listOfListsOfMatchingCharsInScene.Count = " + listOfListsOfMatchingCharsInScene.Count.ToString + vbCrLf)     '13 with MCLRNF1 image
+
+            imgContours = New Mat(imgOriginalScene.Size, DepthType.Cv8U, 3)
+
+            For Each listOfMatchingChars As List(Of PossibleChar) In listOfListsOfMatchingCharsInScene
+                Dim intRandomBlue = random.Next(0, 256)
+                Dim intRandomGreen = random.Next(0, 256)
+                Dim intRandomRed = random.Next(0, 256)
+
+                Dim contours As New VectorOfVectorOfPoint()
+
+                For Each matchingChar As PossibleChar In listOfMatchingChars
+                    contours.Push(matchingChar.contour)
+                Next
+
+                CvInvoke.DrawContours(imgContours, contours, -1, New MCvScalar(CDbl(intRandomBlue), CDbl(intRandomGreen), CDbl(intRandomRed)))
+
+            Next
+            CvInvoke.Imshow("3", imgContours)
+        End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        For Each listOfMatchingChars As List(Of PossibleChar) In listOfListsOfMatchingCharsInScene          'for each group of matching chars
+            Dim possiblePlate = extractPlate(imgOriginalScene, listOfMatchingChars)                         'attempt to extract plate
+
+            If (Not possiblePlate.imgPlate Is Nothing) Then                                                 'if plate was found
+                listOfPossiblePlates.Add(possiblePlate)                                                     'add to list of possible plates
+            End If
+        Next
+
+        Koleso.txtInfo.AppendText(vbCrLf + listOfPossiblePlates.Count.ToString + " possible plates found" + vbCrLf)    'update text box with # of plates found
+
+        If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''''''
+            Koleso.txtInfo.AppendText(vbCrLf)
+            CvInvoke.Imshow("4a", imgContours)
+
+            For i As Integer = 0 To listOfPossiblePlates.Count - 1
+                Dim ptfRectPoints(4) As PointF
+
+                ptfRectPoints = listOfPossiblePlates(i).rrLocationOfPlateInScene.GetVertices()
+
+                Dim pt0 As New Point(CInt(ptfRectPoints(0).X), CInt(ptfRectPoints(0).Y))
+                Dim pt1 As New Point(CInt(ptfRectPoints(1).X), CInt(ptfRectPoints(1).Y))
+                Dim pt2 As New Point(CInt(ptfRectPoints(2).X), CInt(ptfRectPoints(2).Y))
+                Dim pt3 As New Point(CInt(ptfRectPoints(3).X), CInt(ptfRectPoints(3).Y))
+
+                CvInvoke.Line(imgContours, pt0, pt1, SCALAR_RED, 2)
+                CvInvoke.Line(imgContours, pt1, pt2, SCALAR_RED, 2)
+                CvInvoke.Line(imgContours, pt2, pt3, SCALAR_RED, 2)
+                CvInvoke.Line(imgContours, pt3, pt0, SCALAR_RED, 2)
+
+                CvInvoke.Imshow("4a", imgContours)
+                Koleso.txtInfo.AppendText("possible plate " + i.ToString + ", click on any image and press a key to continue . . ." + vbCrLf)
+                CvInvoke.Imshow("4b", listOfPossiblePlates(i).imgPlate)
+                CvInvoke.WaitKey(0)
+            Next
+            Koleso.txtInfo.AppendText(vbCrLf + "plate detection complete, click on any image and press a key to begin char recognition . . ." + vbCrLf + vbCrLf)
+            CvInvoke.WaitKey(0)
+        End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        Return listOfPossiblePlates
+    End Function
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    Function findPossibleCharsInScene(imgThresh As Mat) As List(Of PossibleChar)
+        Dim listOfPossibleChars As List(Of PossibleChar) = New List(Of PossibleChar)      'this is the return value
+
+        Dim imgContours As New Mat(imgThresh.Size(), DepthType.Cv8U, 3)
+        Dim intCountOfPossibleChars As Integer = 0
+
+        Dim imgThreshCopy As Mat = imgThresh.Clone()
+
+        Dim contours As New VectorOfVectorOfPoint()
+
+        CvInvoke.FindContours(imgThreshCopy, contours, Nothing, RetrType.List, ChainApproxMethod.ChainApproxSimple)     'find all contours
+
+        For i As Integer = 0 To contours.Size() - 1                                                     'for each contour
+            If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''
+                CvInvoke.DrawContours(imgContours, contours, i, SCALAR_WHITE)
+            End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+            Dim possibleChar As New PossibleChar(contours(i))
+
+            If (DetectChars.checkIfPossibleChar(possibleChar)) Then         'if contour is a possible char, note this does not compare to other chars (yet) . . .
+                intCountOfPossibleChars = intCountOfPossibleChars + 1       'increment count of possible chars
+                listOfPossibleChars.Add(possibleChar)                       'and add to list of possible chars
+            End If
+
+        Next
+
+        If (Koleso.cbShowSteps.Checked = True) Then ' show steps '''''''''''''''''''''''''''''''''
+            Koleso.txtInfo.AppendText(vbCrLf + "step 2 - contours.Size() = " + contours.Size().ToString + vbCrLf)      '2362 with MCLRNF1 image
+            Koleso.txtInfo.AppendText("step 2 - intCountOfPossibleChars = " + intCountOfPossibleChars.ToString + vbCrLf)     '131 with MCLRNF1 image
+            CvInvoke.Imshow("2a", imgContours)
+        End If ' show steps '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        Return listOfPossibleChars
+    End Function
+
+    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    Function extractPlate(imgOriginal As Mat, listOfMatchingChars As List(Of PossibleChar)) As PossiblePlate
+        Dim possiblePlate As PossiblePlate = New PossiblePlate          'this will be the return value
+
+        'sort chars from left to right based on x position
+        listOfMatchingChars.Sort(Function(firstChar, secondChar) firstChar.intCenterX.CompareTo(secondChar.intCenterX))
+
+        'calculate the center point of the plate
+        Dim dblPlateCenterX As Double = CDbl(listOfMatchingChars(0).intCenterX + listOfMatchingChars(listOfMatchingChars.Count - 1).intCenterX) / 2.0
+        Dim dblPlateCenterY As Double = CDbl(listOfMatchingChars(0).intCenterY + listOfMatchingChars(listOfMatchingChars.Count - 1).intCenterY) / 2.0
+        Dim ptfPlateCenter As New PointF(CSng(dblPlateCenterX), CSng(dblPlateCenterY))
+
+        'calculate plate width and height
+        Dim intPlateWidth As Integer = CInt(CDbl(listOfMatchingChars(listOfMatchingChars.Count - 1).boundingRect.X + listOfMatchingChars(listOfMatchingChars.Count - 1).boundingRect.Width - listOfMatchingChars(0).boundingRect.X) * PLATE_WIDTH_PADDING_FACTOR)
+
+        Dim intTotalOfCharHeights As Integer = 0
+
+        For Each matchingChar As PossibleChar In listOfMatchingChars
+            intTotalOfCharHeights = intTotalOfCharHeights + matchingChar.boundingRect.Height
+        Next
+
+        Dim dblAverageCharHeight = CDbl(intTotalOfCharHeights) / CDbl(listOfMatchingChars.Count)
+
+        Dim intPlateHeight = CInt(dblAverageCharHeight * PLATE_HEIGHT_PADDING_FACTOR)
+
+        'calculate correction angle of plate region
+        Dim dblOpposite As Double = listOfMatchingChars(listOfMatchingChars.Count - 1).intCenterY - listOfMatchingChars(0).intCenterY
+        Dim dblHypotenuse As Double = DetectChars.distanceBetweenChars(listOfMatchingChars(0), listOfMatchingChars(listOfMatchingChars.Count - 1))
+        Dim dblCorrectionAngleInRad As Double = Math.Asin(dblOpposite / dblHypotenuse)
+        Dim dblCorrectionAngleInDeg As Double = dblCorrectionAngleInRad * (180.0 / Math.PI)
+
+        'assign rotated rect member variable of possible plate
+        possiblePlate.rrLocationOfPlateInScene = New RotatedRect(ptfPlateCenter, New SizeF(CSng(intPlateWidth), CSng(intPlateHeight)), CSng(dblCorrectionAngleInDeg))
+
+        Dim rotationMatrix As New Mat()         'final steps are to perform the actual rotation
+        Dim imgRotated As New Mat()
+        Dim imgCropped As New Mat()
+
+        CvInvoke.GetRotationMatrix2D(ptfPlateCenter, dblCorrectionAngleInDeg, 1.0, rotationMatrix)      'get the rotation matrix for our calculated correction angle
+
+        CvInvoke.WarpAffine(imgOriginal, imgRotated, rotationMatrix, imgOriginal.Size)          'rotate the entire image
+
+        'crop out the actual plate portion of the rotated image
+        CvInvoke.GetRectSubPix(imgRotated, possiblePlate.rrLocationOfPlateInScene.MinAreaRect.Size, possiblePlate.rrLocationOfPlateInScene.Center, imgCropped)
+
+        possiblePlate.imgPlate = imgCropped         'copy the cropped plate image into the applicable member variable of the possible plate
+
+        Return possiblePlate
+    End Function
+
+End Module
---
+++ Koleso.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Koleso
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Koleso))
+        Me.tableLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.ibOriginal = New System.Windows.Forms.PictureBox()
+        Me.cbShowSteps = New System.Windows.Forms.CheckBox()
+        Me.txtInfo = New System.Windows.Forms.TextBox()
+        Me.lblChosenFile = New System.Windows.Forms.Label()
+        Me.ofdOpenFile = New System.Windows.Forms.OpenFileDialog()
+        Me.tableLayoutPanel.SuspendLayout()
+        CType(Me.ibOriginal, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tableLayoutPanel
+        '
+        Me.tableLayoutPanel.ColumnCount = 3
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.14256!))
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.85744!))
+        Me.tableLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.tableLayoutPanel.Controls.Add(Me.btnOpenFile, 0, 0)
+        Me.tableLayoutPanel.Controls.Add(Me.ibOriginal, 0, 1)
+        Me.tableLayoutPanel.Controls.Add(Me.cbShowSteps, 2, 0)
+        Me.tableLayoutPanel.Controls.Add(Me.txtInfo, 0, 2)
+        Me.tableLayoutPanel.Controls.Add(Me.lblChosenFile, 1, 0)
+        Me.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tableLayoutPanel.Location = New System.Drawing.Point(0, 0)
+        Me.tableLayoutPanel.Name = "tableLayoutPanel"
+        Me.tableLayoutPanel.RowCount = 3
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.0!))
+        Me.tableLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30.0!))
+        Me.tableLayoutPanel.Size = New System.Drawing.Size(954, 500)
+        Me.tableLayoutPanel.TabIndex = 0
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnOpenFile.AutoSize = True
+        Me.btnOpenFile.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.btnOpenFile.Location = New System.Drawing.Point(3, 3)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(126, 34)
+        Me.btnOpenFile.TabIndex = 0
+        Me.btnOpenFile.Text = "Открыть файл"
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'ibOriginal
+        '
+        Me.ibOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tableLayoutPanel.SetColumnSpan(Me.ibOriginal, 3)
+        Me.ibOriginal.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ibOriginal.Enabled = False
+        Me.ibOriginal.Location = New System.Drawing.Point(3, 43)
+        Me.ibOriginal.Name = "ibOriginal"
+        Me.ibOriginal.Size = New System.Drawing.Size(948, 316)
+        Me.ibOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.ibOriginal.TabIndex = 2
+        Me.ibOriginal.TabStop = False
+        '
+        'cbShowSteps
+        '
+        Me.cbShowSteps.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cbShowSteps.AutoSize = True
+        Me.cbShowSteps.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.cbShowSteps.Location = New System.Drawing.Point(820, 9)
+        Me.cbShowSteps.Name = "cbShowSteps"
+        Me.cbShowSteps.Size = New System.Drawing.Size(131, 22)
+        Me.cbShowSteps.TabIndex = 3
+        Me.cbShowSteps.Text = "показать шаги"
+        Me.cbShowSteps.UseVisualStyleBackColor = True
+        '
+        'txtInfo
+        '
+        Me.tableLayoutPanel.SetColumnSpan(Me.txtInfo, 3)
+        Me.txtInfo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.txtInfo.Location = New System.Drawing.Point(3, 365)
+        Me.txtInfo.Multiline = True
+        Me.txtInfo.Name = "txtInfo"
+        Me.txtInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtInfo.Size = New System.Drawing.Size(948, 132)
+        Me.txtInfo.TabIndex = 4
+        Me.txtInfo.WordWrap = False
+        '
+        'lblChosenFile
+        '
+        Me.lblChosenFile.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblChosenFile.AutoSize = True
+        Me.lblChosenFile.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.lblChosenFile.Location = New System.Drawing.Point(135, 11)
+        Me.lblChosenFile.Name = "lblChosenFile"
+        Me.lblChosenFile.Size = New System.Drawing.Size(679, 18)
+        Me.lblChosenFile.TabIndex = 1
+        Me.lblChosenFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'ofdOpenFile
+        '
+        Me.ofdOpenFile.FileName = "OpenFileDialog1"
+        '
+        'Koleso
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(954, 500)
+        Me.Controls.Add(Me.tableLayoutPanel)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Koleso"
+        Me.Text = "Koleso"
+        Me.tableLayoutPanel.ResumeLayout(False)
+        Me.tableLayoutPanel.PerformLayout()
+        CType(Me.ibOriginal, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tableLayoutPanel As TableLayoutPanel
+    Friend WithEvents btnOpenFile As Button
+    Friend WithEvents lblChosenFile As Label
+    Friend WithEvents ibOriginal As PictureBox
+    Friend WithEvents cbShowSteps As CheckBox
+    Friend WithEvents txtInfo As TextBox
+    Friend WithEvents ofdOpenFile As OpenFileDialog
+End Class
